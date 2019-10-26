729b5e4c7160e37d5490c5f3cb27dd21231fefd2
doanguyen <dungnv2410@gmail.com>
2019-10-26 17:26:42 +0200

    First init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Dashboard/>
+  </div>
+</template>
+
+<script>
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
